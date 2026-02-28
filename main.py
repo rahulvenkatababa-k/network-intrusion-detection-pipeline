6f40349c9e39839928a4240c66d78eb812b278e5
rahulvenkatababa-k <rahulvenkatababa@gmail.com>
2026-02-28 22:14:29 +0530

    Data transformation Completed

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_tranformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.entity.config_entity import (TrainingPipelineConfig, DataIngestionConfig, 
-                                                  DataValidationConfig)
+                                                  DataValidationConfig, DataTransformationConfig)
 from networksecurity.logging.logger import logging
 import os
 import sys
@@ -25,6 +26,15 @@ if __name__=='__main__':
         data_validation_artifact = data_validation.initiate_data_validation()
         logging.info("Data validation completed")
         print(data_validation_artifact)
+
+        data_tranformation_config = DataTransformationConfig(trainingpipelineconfig)
+        data_transformation = DataTransformation(data_transformation_config=data_tranformation_config,
+                                                  data_validation_artifact=data_validation_artifact)
+        logging.info("Initiate the Data Transformation")
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        logging.info("Data Transformation compeltd")
+        print(data_transformation_artifact)
+        
         
 
     except Exception as e:
---
+++ networksecurity/components/data_tranformation.py
@@ -0,0 +1,82 @@
+import sys, os
+import pandas as pd
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn.impute import KNNImputer
+
+from networksecurity.constant.training_pipeline import (TARGET_COLUMN, DATA_TRANSFORMATION_IMPUTER_PARAMS)
+from networksecurity.entity.artifact_entity import (DataTransformationArtifact, DataValidationArtifact)
+from networksecurity.entity.config_entity import DataTransformationConfig
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+from networksecurity.utils.main_utils.utils import (save_numpy_array_data, save_object)
+
+class DataTransformation:
+    def __init__(self, data_validation_artifact: DataValidationArtifact,
+                        data_transformation_config: DataTransformationConfig):
+        try:
+            self.data_validation_artifact:DataValidationArtifact = data_validation_artifact
+            self.data_transformation_config:DataTransformationConfig = data_transformation_config
+        except Exception as e:
+            raise NetworkSecurityException(e,sys) from e
+    
+    @staticmethod
+    def read_data(file_path: str)-> pd.DataFrame:
+        try:
+            return pd.read_csv(file_path)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+    
+    def get_data_transformer_object(self) -> Pipeline:
+
+        logging.info("Entered get_data_transformer_object method of transformation classs")
+        try:
+            imputer = KNNImputer(**DATA_TRANSFORMATION_IMPUTER_PARAMS)
+            logging.info(f" Initialized knn with {DATA_TRANSFORMATION_IMPUTER_PARAMS}")
+            processer:Pipeline = Pipeline([("imputer",imputer)])
+
+            return processer
+
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+    
+    def initiate_data_transformation(self)-> DataTransformationArtifact:
+        logging.info("Entered initiate_data_transformation method of DataTransformation class")
+        try:
+            logging.info("Started data transformation")
+            train_df = DataTransformation.read_data(self.data_validation_artifact.valid_train_file_path)
+            test_df = DataTransformation.read_data(self.data_validation_artifact.valid_test_file_path)
+
+            # training data frame
+            input_feature_train_df = train_df.drop(columns=[TARGET_COLUMN],axis=1)
+            target_feature_train_df = train_df[TARGET_COLUMN]
+            target_feature_train_df = target_feature_train_df.replace(-1, 0)
+            # Test data frame
+            input_feature_test_df = test_df.drop(columns=[TARGET_COLUMN],axis=1)
+            target_feature_test_df = test_df[TARGET_COLUMN]
+            target_feature_test_df = target_feature_test_df.replace(-1,0)
+
+            preprocesser = self.get_data_transformer_object()
+            preprocess_objecct = preprocesser.fit(input_feature_train_df)
+            transformed_input_train_feature = preprocess_objecct.transform(input_feature_train_df)
+            transformed_input_test_feature = preprocess_objecct.transform(input_feature_test_df)
+
+            train_arr = np.c_[transformed_input_train_feature, np.array(target_feature_train_df)]
+            test_arr = np.c_[transformed_input_test_feature,np.array(target_feature_test_df)]
+
+            # save numpy array data
+            save_numpy_array_data(self.data_transformation_config.transformed_train_file_path,array=train_arr)
+            save_numpy_array_data(self.data_transformation_config.transformed_test_file_path,array=test_arr)
+            save_object(self.data_transformation_config.transformed_object_file_path,preprocess_objecct)
+
+            # preparing artifacts
+            data_transformation_artifact  = DataTransformationArtifact(
+                transformed_object_file_path=self.data_transformation_config.transformed_object_file_path,
+                transformed_train_file_path=self.data_transformation_config.transformed_train_file_path,
+                transformed_test_file_path=self.data_transformation_config.transformed_test_file_path
+            )
+
+            return data_transformation_artifact
+
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
